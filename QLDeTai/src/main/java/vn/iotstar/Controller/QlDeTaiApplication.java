a14cbc36ec2d7836975a23a8b52d166fb5da40be
meowmilk <toannguyen7202@gmail.com>
2022-11-20 21:38:47 +0700

    20/11 create proj

---
+++ QLDeTai/src/main/java/vn/iotstar/Controller/QlDeTaiApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstar.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlDeTaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlDeTaiApplication.class, args);
+	}
+
+}
---
+++ QLDeTai/src/test/java/vn/iotstar/Controller/QlDeTaiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.iotstar.Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlDeTaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
