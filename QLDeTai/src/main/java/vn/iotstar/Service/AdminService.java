7a88cc41a5e4ebf0e82610d62848c7feb3b130a3
CongThanh27 <nguyencongthanh280702@gmail.com>
2022-11-20 23:13:44 +0700

    20/11

---
+++ QLDeTai/src/main/java/vn/iotstar/Service/AdminService.java
@@ -0,0 +1,15 @@
+package vn.iotstar.Service;
+
+public interface AdminService {
+
+	void finalize() throws Throwable;
+
+	String toString();
+
+	Object clone() throws CloneNotSupportedException;
+
+	boolean equals(Object obj);
+
+	int hashCode();
+
+}
