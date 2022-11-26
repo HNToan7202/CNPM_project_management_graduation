6883a945efcae9ca0b2a92465b74a39fede4189a
Nguyễn Hoàng Toàn <toannguyen7202@gmail.com>
2022-11-26 23:38:33 +0700

    pull code 26/11

--- ManageProject/src/main/java/vn/iotstar/Controller/Admin/LectureController.java
+++ ManageProject/src/main/java/vn/iotstar/Controller/Admin/LectureController.java
@@ -93,7 +93,7 @@ public class LectureController {
 
 	@GetMapping("search")
 	public String search() {
-		return "admin/lecture/search";
+		return "admin/add/GiangVien";
 	}
 
 	@GetMapping("delete/{id}")
